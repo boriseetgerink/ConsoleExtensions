1b6d4851c1d45d2155b0e505be0a58b7634fe656
Boris Eetgerink <47523920+boriseetgerink@users.noreply.github.com>
2023-06-27 10:03:43 +0200

    Project setup.

---
+++ App/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BorisEetgerink.ConsoleExtensions;
+
+string? line = ConsoleExtensions.ReadLine("What is your favorite color? >", "Green");
+Console.WriteLine($"|{line}|");
---
+++ BorisEetgerink.ConsoleExtensions/ReadLine.cs
@@ -0,0 +1,10 @@
+﻿namespace BorisEetgerink.ConsoleExtensions
+{
+    public static class ConsoleExtensions
+    {
+        public static string ReadLine(string prompt, string defaultInput)
+        {
+            return defaultInput;
+        }
+    }
+}
