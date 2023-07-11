10a0146bba5bb013819bc0a4154d4ec9c47a7f15
Boris Eetgerink <47523920+boriseetgerink@users.noreply.github.com>
2023-07-11 07:55:41 +0200

    Add ReadInt methods.

---
+++ BorisEetgerink.ConsoleExtensions/ReadInt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BorisEetgerink.ConsoleExtensions
+{
+    public static partial class ConsoleExtensions
+    {
+        public static int ReadInt() => ReadInt(string.Empty, null);
+
+        public static int ReadInt(string prompt) => ReadInt(prompt, null);
+
+        public static int ReadInt(string prompt, int defaultInput) => ReadInt(prompt, (int?)defaultInput);
+
+        private static int ReadInt(string prompt, int? defaultInput)
+        {
+            if (prompt == null)
+            {
+                throw new ArgumentNullException(nameof(prompt));
+            }
+
+            string defaultInputString = defaultInput?.ToString() ?? string.Empty;
+
+            return defaultInput ?? 0;
+        }
+    }
+}
