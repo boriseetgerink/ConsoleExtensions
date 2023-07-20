762a6ad1219e8b23836786c43a22e3b7ef2f85d8
Boris Eetgerink <47523920+boriseetgerink@users.noreply.github.com>
2023-07-20 14:40:45 +0200

    Move Prompt options to shared base class.

--- BorisEetgerink.ConsoleExtensions/Options/ConfirmOptions.cs
+++ BorisEetgerink.ConsoleExtensions/Options/ConfirmOptions.cs
@@ -1,24 +1,10 @@
-﻿using System;
-
-namespace BorisEetgerink.ConsoleExtensions.Options
+﻿namespace BorisEetgerink.ConsoleExtensions.Options
 {
     /// <summary>
     /// Confirm options.
     /// </summary>
-    public class ConfirmOptions
+    public class ConfirmOptions : PromptOptions
     {
-        private string _prompt = string.Empty;
-
-        /// <summary>
-        /// Gets or sets the prompt.
-        /// </summary>
-        /// <exception cref="ArgumentNullException">If prompt is null.</exception>
-        public string Prompt
-        {
-            get => _prompt;
-            set => _prompt = value ?? throw new ArgumentNullException(nameof(value), "Prompt may not be null.");
-        }
-
         /// <summary>
         /// Gets or sets the default choice. Defaults to True (yes).
         /// </summary>
---
+++ BorisEetgerink.ConsoleExtensions/Options/PromptOptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BorisEetgerink.ConsoleExtensions.Options
+{
+    /// <summary>
+    /// Prompt options.
+    /// </summary>
+    public abstract class PromptOptions
+    {
+        private string _prompt = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the prompt.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">If prompt is null.</exception>
+        public string Prompt
+        {
+            get => _prompt;
+            set => _prompt = value ?? throw new ArgumentNullException(nameof(value), "Prompt may not be null.");
+        }
+    }
+}
